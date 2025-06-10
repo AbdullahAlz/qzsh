28b4415d6ed7f64eda838ea0c5347bee53c59e5e
Abdullah Alzein <abdullah.alzein1@gmail.com>
2025-06-10 21:34:25 +0200

    generalize .fzf.zsh

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,7 +1,7 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/user/.config/qzsh/fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/home/user/.config/qzsh/fzf/bin"
+if [[ ! "$PATH" == *$HOME/.config/qzsh/fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}$HOME.config/qzsh/fzf/bin"
 fi
 
 source <(fzf --zsh)
