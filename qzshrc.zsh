f5f8cddd615efbabaf28970de4e5517cad27a468
Abdullah Alzein <abdullah.alzein1@gmail.com>
2025-07-26 22:27:16 +0200

    update ghosttyConfig, add function to render colors

--- qzshrc.zsh
+++ qzshrc.zsh
@@ -156,6 +156,14 @@ ddg() {
 wiki() {
     open "https://en.wikipedia.org/wiki/Special:Search?search=${(j:+:)@}"
 }
+showallcolors() {
+    for i in {0..255}; do
+    printf "\e[48;5;${i}m %3d \e[0m" $i
+    if (( ($i + 1) % 16 == 0 )); then
+        echo
+    fi
+done
+}
 
 alias e="exit"
 alias ip="ip --color=auto"
